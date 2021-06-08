4b1c370af0f07eb7c142dbfebb9c109e3e76045c
randellz <randellz@oregonstate.edu>
2021-06-08 07:49:23 -0700

    polished Chlorophyll code for single time point

--- Code/Chlorophyll.R
+++ Code/Chlorophyll.R
@@ -27,6 +27,7 @@ library("sf")
 library("remotes")
 library("gdalUtils")
 library("rgeos")
+library("lwgeom")
 
 setwd("D:/OneDrive/Active_Projects/Substrate_Complexity/Data/Chlorophyll")
 
@@ -40,7 +41,7 @@ my.theme = theme(panel.grid.major = element_blank(),
                  plot.title = element_text(size=16))
 
 graphics.off()
-windows(h=8,w=8, record=TRUE)
+windows(h=6.5,w=8, record=TRUE)
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -50,22 +51,18 @@ windows(h=8,w=8, record=TRUE)
 ## open and plot netCDF file ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
 nc_data <- nc_open("grain3.nc")
 
-
 ## saves text file with metadata 
 {sink("grain3_metadata.txt")
   print(nc_data)
   sink()}
-
 
 ## extract chlorophyll, latitude, and longitude information
 chlor <- ncvar_get(nc_data, "geophysical_data/chlor_a")
 lat <- ncvar_get(nc_data, "navigation_data/latitude")
 long <- ncvar_get(nc_data, "navigation_data/longitude")
 
-
 ## close netCDF file 
 nc_close(nc_data)
-
 
 ## open coastline data
 coastlines <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp")
@@ -80,45 +77,40 @@ coastlines <- readOGR("ne-coastlines-10m/ne_10m_coastline.shp")
 ## format, and should instead be treated as vectors. 
 xy <- cbind(as.vector(long), as.vector(lat))
 
-
 ## visualize these vectors, where the lat long coordinates plot as the skewed 
 ## image that the previous raster format was unable to achieve
 i <- seq(1, nrow(xy), 500)
 plot(xy[i,])
-
 
 ## create RasterLayer out of lat / long coordinates 
 ## the stated resolution of r when resolution is undefined 
 ## (verify the above with r <- raster(extent(xy)), then run "r")
 natural_res <- c(2.84775, 2.12091)
 
-
 ## scale the natural resolution by res
-res <- 115
+res <- 120
 new_res <- natural_res / res
-
 
 ## creater raster layer
 r <- raster(extent(xy), res=new_res)
-
 
 ## transform chlor values into Raster Layer 
 x <- rasterize(xy, r, as.vector(chlor), 
                fun=mean)
 
-
 ## broader Channel Islands and southern CA bight
 box1 <- as(extent(-121.0, -118.0, 32.4, 35.0), 'SpatialPolygons')
-
 
 ## zoom into SNI
 box2 <- as(extent(-119.9199, -119.0736, 32.9596, 33.5081), 'SpatialPolygons')
 
+## narrow around SNI 
+box3 <- as(extent(-119.8, -119.2, 33.05, 33.45), 'SpatialPolygons')
 
 ## apply cutout to chlor, lat long raster & coastline data  
-p1 <- crop(x, box2)
-SNI <- crop(coastlines, box2)
-plot(p1, zlim=c(0,10))
+p1 <- crop(x, box3)
+SNI <- crop(coastlines, box3)
+plot(p1)
 lines(SNI)
 
 ## MODIS implicit CRS -- explicitly set
@@ -126,13 +118,8 @@ MODIS_crs <- "+proj=longlat +datum=WGS84 +no_defs"
 crs(x) <- MODIS_crs
 
 
-## check crs
-#st_crs(x)
-#st_crs(SNI)
-
-
 ## plot 
-plot(p1, zlim=c(0,10))
+plot(p1)
 lines(SNI)
 abline(v=-119.4992, h=33.2465, col="blue")
 ## END additional data processing ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -147,7 +134,6 @@ two_km_buffer <- raster::buffer(SNI, width=.03)
 plot(two_km_buffer, col="cyan")
 lines(SNI)
 
-
 ## use raster::erase to only retain outside of island
 ## need to convert SpatialLinesDataFrame --> sf --> SpatialPolygonDataFrame
 SNI_sf <- st_as_sf(SNI)
@@ -156,9 +142,9 @@ SNI_spatialpoly <- as(SNI_poly, "Spatial")
 buff <- raster::erase(two_km_buffer, SNI_spatialpoly)
 plot(buff, col="#03A89E88")
 
-
 ## plot Cholorphyll data and 2km buffer
-plot(p1, zlim=c(0,10), main="3km buffer to extract Chlorophyll values from around the outside of SNI")
+plot(p1,  
+     main="3km buffer to extract Chlorophyll values from around the outside of SNI")
 plot(buff, col="#03A89E50", add=TRUE)
 ## END buffer creation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -166,23 +152,102 @@ plot(buff, col="#03A89E50", add=TRUE)
 
 
 
+## CREATE LINES TO PARTITION BUFFER AND CREATE FOUR REGIONS ~~~~~~~~~~~~~~~~~~~~
+## coordinates centered around SNI lat / long 
+#l1 <-  rbind(c(-119.4992, 33.1), c(-119.4992, 33.4))
+#l2 <- rbind(c(-119.8, 33.2468), c(-119.2, 33.2468))
+
+
+## coordinates manually determined lines based on exposure
+l1 <-  rbind(c(-119.312897, 33.18), c(-119.695722, 33.32))
+l2 <- rbind(c(-119.441736, 33.38125), c(-119.570142, 33.125417))
+
+## coordintes --> Formal class Line object 
+S1 <- Line(l1)
+S2 <- Line(l2)
+S3 <- Lines(list(S1, S2), ID="a")
+
+## Formal class Line --> SpatialLines 
+quadLines <- SpatialLines(list(S3))
+
+## check lines 
+plot(p1)
+lines(SNI)
+plot(buff, add=T)
+lines(quadLines)
+
+## remove coordinate system from buffer layer to facilitate next steps
+crs(buff) <- NA
+crs(quadLines) <- NA
+
+## intersect buffer with quadLines, buffer the intersection, and split the regions
+intersect1 <- gIntersection(buff, quadLines)
+intersect2 <- gBuffer(intersect1, width=0.0001) 
+regions <- gDifference(buff, intersect2)
+
+## extract the four new regions 
+sw <- SpatialPolygons(list(Polygons(list(regions@polygons[[1]]@Polygons[[1]]), "1")))
+nw <- SpatialPolygons(list(Polygons(list(regions@polygons[[1]]@Polygons[[2]]), "2")))
+ne <- SpatialPolygons(list(Polygons(list(regions@polygons[[1]]@Polygons[[3]]), "3")))
+se <- SpatialPolygons(list(Polygons(list(regions@polygons[[1]]@Polygons[[4]]), "4")))
+
+## plot and check
+plot(p1, main="3km buffer split into four regions")
+lines(SNI)
+plot(sw, add=T, col="#B2222250")
+plot(nw, add=T, col="#476A3450")
+plot(ne, add=T, col="#FF610350") 
+plot(se, add=T, col="#00808050")
+## END buffer regions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
 ## Extract and visualize Chlorophyll data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## use spatial polygon specified by coordinates to extract data 
-d1 <- raster::extract(p1, buff)
-d1 <- na.omit(as.data.frame(d1))
-names(d1)[1]<-"dat"
+sw_chlor <- as.data.frame(raster::extract(p1, sw))
+nw_chlor <- as.data.frame(raster::extract(p1, nw))
+ne_chlor <- as.data.frame(raster::extract(p1, ne))
+se_chlor <- as.data.frame(raster::extract(p1, se))
+
+## site column 
+sw_chlor$region <- "SouthWest"
+nw_chlor$region <- "NorthWest"
+ne_chlor$region <- "NorthEast"
+se_chlor$region <- "SouthEast"
+
+## rename column with chlorophyll values
+names(sw_chlor)[1]<-"dat" 
+names(nw_chlor)[1]<-"dat"
+names(ne_chlor)[1]<-"dat"
+names(se_chlor)[1]<-"dat"
+
+## single data frame with all chlorophyll data
+chlorophyll_dat <- rbind(sw_chlor, nw_chlor, ne_chlor, se_chlor)
+
+## reorder factors into desired order for plot 
+chlorophyll_dat$region <- factor(chlorophyll_dat$region, levels=c("NorthWest", "NorthEast",
+## END chlorophyll data extract ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~           
+
+                                                       
 
 
-## plot
-v1 <- ggplot(d1, aes(dat)) + my.theme +
-  geom_histogram(binwidth=.25, color="black", fill="#388E8E") +
-  xlab("Chlorophyll_a mg / m^-3") 
+                                                                                                           "SouthWest", "SouthEast")) 
+## Plot ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
+graphics.off()
+windows(h=6,w=10, record=TRUE)
+
+## custom color pallet
+my.pal <- c("#476A34","#FF6103",
+            "#B22222", "#008080")
+
+## frequency histograms 
+v1 <- ggplot(chlorophyll_dat, aes(dat, fill=region)) + geom_histogram(binwidth=.5) +
+  xlab("Chlorophyll_a mg / m^-3") + my.theme + facet_wrap(~ region) +
+  scale_fill_manual(values = my.pal)
 print(v1)
 ## END data extract ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## END script ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ##
-
-
-
-
 
