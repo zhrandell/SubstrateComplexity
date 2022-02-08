8189b29fbb0498a9a6d1ece3cb058ab27523f802
randellz <randellz@oregonstate.edu>
2022-02-08 07:27:53 -0800

    condensed code

--- Code/figSOM_waveHeight.R
+++ Code/figSOM_waveHeight.R
@@ -10,29 +10,10 @@
 ## initiate ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 rm(list = ls())
 
-library(ggplot2)
-library(dplyr)
-library(vegan)
-library(MASS)
-library(fitdistrplus)
-library(gridExtra)
-library(RColorBrewer)
-library(ggpubr)
-library(egg)
-library(gtable)
-library(grid)
-library(magick)
-library(magrittr)
-library(here)
-library(pryr)
-library(ggbeeswarm)
-library(WaveletComp)
-library(devtools)
+library(tidyverse)
 
-
-setwd("D:/OneDrive/Active_Projects/Substrate_Complexity/Data")
+setwd("D:/OneDrive/Active_Projects/SubstrateComplexity/Data")
 dat <- read.csv("WaveHeights.csv", header = TRUE)
-
 
 ## set up custom ggplot theme 
 my.theme = theme(panel.grid.major = element_blank(), 
@@ -45,10 +26,7 @@ my.theme = theme(panel.grid.major = element_blank(),
                  legend.text = element_text(size=14), 
                  legend.title = element_blank(), 
                  legend.position = c(0.85, 0.85))
-
-graphics.off()
-windows(h=5,w=8, record=TRUE)
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## END initiate ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
@@ -67,118 +45,55 @@ dat <- filter(dat, SU > 3364864 & SU < 28870656)
 dat$logWV <- log10(dat$WaveHeight)
 
 
-## subset by site
-NF <- filter(dat, Site %in% c("NavFac"))
-WE <- filter(dat, Site %in% c("WestEnd"))
-Day <- filter(dat, Site %in% c("Daytona"))
-ED <- filter(dat, Site %in% c("EastDutch"))
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## filter by site
+filter.site <- function(x){filter(dat, Site %in% c(x))}
+NF <- filter.site("NavFac")
+WE <- filter.site("WestEnd")
+Day <- filter.site("Daytona")
+ED <- filter.site("EastDutch")
+## END data formatting ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
 
 
-## Plot ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## custom color pal matching ms site colors. NF, blend(WEU & WEK), Day, ED
-pal_sites <- c("#BE2625", "#E88600", "#608341", "#00536f") #BE2625 used for tint and shade creation
-#pal_sites2 <- c("#BE2625", "#E88600", "#6f006b", "#00536f") #BE2625 used for tint and shade creation
+## custom graphing params ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cols <- scale_color_manual(values=c("#BE2625", "#E88600", "#608341", "#00536f")) 
+x.lab.ts <- scale_x_continuous(n.breaks=12,labels=c("null","Nov","Dec","Jan","Feb","Mar","Apr","May","June","July","Aug", "Sep","")) 
+x.lab.kd <- scale_x_continuous(labels=c("null", "0.40", "1", "2.5", "6.3")) 
+## END custom params ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-#6f006b
-## graphing window
+
+
+
+
+## plot time series and kernal densities ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## plot time series of untransformed wave height
+p1 <- ggplot(dat, aes(SU, WaveHeight, color=Site)) + geom_line(alpha=.7) + cols + x.lab.ts + 
+  my.theme + theme(axis.title.x=element_blank()) + ylab("Wave height (meters)")
+
+
+## plot kernal density use log base-10 transformed wave data
+p2 <- ggplot(dat, aes(logWV, fill=Site)) + geom_density(position="identity", color="black", alpha=0.3) + 
+  my.theme + x.lab.kd + cols + xlab("Wave height (meters)") + guides(fill=guide_legend(order=1))
+
+
+## plot wave height time series
 graphics.off()
 windows(w=12,h=4,record=TRUE)
-
-
-## all sites 
-p5 <- ggplot(dat, aes(SU, WaveHeight, color=Site)) +
-  geom_line(alpha=.7) +
-  scale_colour_manual(values=pal_sites) +
-  scale_x_continuous(n.breaks=12, labels=c("null", 
-                                           "Nov","Dec","Jan","Feb","Mar","Apr","May","June","July","Aug", "Sep",
-                                           "")) +
-  my.theme + theme(axis.title.x=element_blank()) + ylab("Wave height (meters)")
-print(p5)
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-## Kernal densities ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## graphing window
-graphics.off()
-windows(w=8,h=5,record=TRUE)
-
-## use logWV for logged data, and WaveHeight for "normal" scale (meters)
-p7 <- ggplot(dat, aes(logWV, fill=Site)) +
-  geom_density(position="identity", color="black", alpha=0.3) + 
-  my.theme +
-  scale_fill_manual(values=pal_sites) +
-  xlab("Wave height (meters)") +
-  scale_x_continuous(labels=c("null", "0.40", "1", "2.5", "6.3")) +
-  guides(fill=guide_legend(order=1))
-print(p7)
-## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-
-
-
-## does wave height data follow a power relationship? ~~~~~~~~~~~~~~~~~~~~~~~~~~
-## calcuate empirical cumulative density function and extract and sort values
-NF_ecdf <- data.frame(x=unique(NF$logWV), 
-                     y=ecdf(NF$logWV)(unique(NF$logWV))*length(NF$logWV))
-WE_ecdf <- data.frame(x=unique(WE$logWV), 
-                     y=ecdf(WE$logWV)(unique(WE$logWV))*length(WE$logWV))
-ED_ecdf <- data.frame(x=unique(ED$logWV), 
-                     y=ecdf(ED$logWV)(unique(ED$logWV))*length(ED$logWV))
-Day_ecdf <- data.frame(x=unique(Day$logWV), 
-                     y=ecdf(Day$logWV)(unique(Day$logWV))*length(Day$logWV))
-
-
-## rescale extracted cdf values to 0-1 scale
-NF_ecdf$y <- scale(NF_ecdf$y, center=min(NF_ecdf$y), scale=diff(range(NF_ecdf$y)))
-WE_ecdf$y <- scale(WE_ecdf$y, center=min(WE_ecdf$y), scale=diff(range(WE_ecdf$y)))
-ED_ecdf$y <- scale(ED_ecdf$y, center=min(ED_ecdf$y), scale=diff(range(ED_ecdf$y)))
-Day_ecdf$y <- scale(Day_ecdf$y, center=min(Day_ecdf$y), scale=diff(range(Day_ecdf$y)))
-
-
-## take the inverse of a cdf, such that the p(x) > or = log wave event
-NF_ecdf$inv_y <- ((NF_ecdf$y - max(NF_ecdf$y)) * (-1))
-WE_ecdf$inv_y <- ((WE_ecdf$y - max(WE_ecdf$y)) * (-1))
-ED_ecdf$inv_y <- ((ED_ecdf$y - max(ED_ecdf$y)) * (-1))
-Day_ecdf$inv_y <- ((Day_ecdf$y - max(Day_ecdf$y)) * (-1))
-
-
-## add site name to new data frames
-NF_ecdf$Site <- "NavFac"
-WE_ecdf$Site <- "WestEnd"
-ED_ecdf$Site <- "EastDutch"
-Day_ecdf$Site <- "Daytona"
-
-
-## combine cfd data frames into single df & and reorder sites in order to plot properly 
-dat_ecdf <- rbind(NF_ecdf, WE_ecdf, ED_ecdf, Day_ecdf)
-dat_ecdf$Site <- factor(dat_ecdf$Site, levels=c("NavFac", "WestEnd", "Daytona", "EastDutch"))
-
-
-## plot all 
-p1 <- ggplot(dat_ecdf, aes(x, inv_y, color=Site)) + my.theme +
-  geom_line(lwd=1, alpha=.8) +
-  scale_color_manual(values=pal_sites) +
-  xlab("Wave height (meters)") + ylab("inverse empirical CDF") +
-  scale_x_continuous(labels=c("null", "0.40", "1", "2.5", "6.3"))
-  
-  #ggtitle("probability of a wave event of equal or greater size relative to log wave height")
-
 print(p1)
-## End script ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+## plot wave height kernal densities
+windows(w=8,h=5,record=TRUE)
+print(p2)
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
 
 
-## KS tests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## KS tests to test kernal density distributions between-sites ~~~~~~~~~~~~~~~~~
 NF_num <- NF$logWV
 WE_num <- WE$logWV
 Day_num <- Day$logWV
@@ -191,10 +106,46 @@ ks.test(NF_num, Day_num)
 ks.test(WE_num, ED_num)
 ks.test(WE_num, Day_num)
 ks.test(Day_num, ED_num)
+## END KS tests ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
 
 
+## calcuate empirical cumulative density function and extract/sort/process values 
+ecdf.f <- function(dat, site.name){
+  t1 <- data.frame(x=unique(dat$logWV), y=ecdf(dat$logWV)(unique(dat$logWV))*length(dat$logWV))
+  t2 <- scale(t1$y, center=min(t1$y), scale=diff(range(t1$y)))
+  t3 <- ((t2 - max(t2)) * (-1))
+  t4 <- cbind(t1, t2, t3)
+  names(t4)[1:4] <- c("x", "unscaled_y", "y", "inv_y")
+  t4$Site <- site.name
+  out <- return(t4)
+}
 
 
+## apply ecdf.f() function to data
+NF.e <- ecdf.f(NF, "NavFac")
+WE.e <- ecdf.f(WE, "WestEnd")
+ED.e <- ecdf.f(ED, "EastDutch")
+Day.e <- ecdf.f(Day, "Daytona")
+
+
+## combine cfd data frames into single df & and reorder sites in order to plot properly 
+dat.e <- rbind(NF.e, WE.e, ED.e, Day.e)
+dat.e$Site <- factor(dat.e$Site, levels=c("NavFac", "WestEnd", "Daytona", "EastDutch"))
+
+
+## plot all 
+p3 <- ggplot(dat.e, aes(x, inv_y, color=Site)) + my.theme + geom_line(lwd=1, alpha=.8) +
+  xlab("Wave height (meters)") + ylab("inverse empirical CDF") + cols + x.lab.kd
+print(p3)
+## END ecdf plot~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+
+
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## END of script ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
